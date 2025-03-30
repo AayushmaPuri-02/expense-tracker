4de6713c8c163a8a47c97870a7d570be293b2d5b
AayushmaPuri-02 <aayushmapuri0301@gmail.com>
2025-03-30 08:58:15 +0545

    created a basic homepage. backend yet to be done

--- app.js
+++ app.js
@@ -89,10 +89,7 @@ app.use((req, res, next) => {
 // })
 app.use("/expenses", expenses);
 app.use("/", userRouter);
-// This should go at the end, AFTER all other routes
-app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page not found!"));
-});
+
 
 app.use((err,req,res,next)=>{
     let {statusCode = 500, message ="something went wrong"} = err;
@@ -119,6 +116,10 @@ app.get("/", async (req, res) => {
     const reviews = await Review.find({});
     console.log(reviews);
     res.render("expenses/home.ejs", { reviews });
+});
+// This should go at the end, AFTER all other routes
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page not found!"));
 });
 
 app.listen(8080, ()=>{
